52f192ef10802b8d2210e10db5fe1cba5b3bdda4
JENNIFER ZHENG <jenniferzyy1012@gmail.com>
2019-11-20 18:35:01 -0500

    initiate files

---
+++ data/uk_lower/Elections/uk_elections.R
@@ -0,0 +1,27 @@
+# remove objects
+rm(list=ls())
+# detach all libraries
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats","package:graphics","package:grDevices","package:utils","package:datasets","package:methods","package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:",search()))==1,TRUE,FALSE)]
+  package.list <- setdiff(package.list,basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
+}
+detachAllPackages()
+
+# load libraries
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+lapply(c("stringr", "dplyr", "plyr", "tidyverse", "rvest", "zoo", "lubridate", "XML"), pkgTest)
+
+# Set WD
+setwd("~/GitHub/CompLegFall2019/data/uk_lower/Elections")
+
+# Read downloaded data
+ele <- read.csv("1918-2017election_results.csv")
+
