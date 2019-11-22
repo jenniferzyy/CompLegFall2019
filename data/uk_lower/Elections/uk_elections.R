48e87f63ee6649bfb9a2836bce2fc60cebf71f17
JENNIFER ZHENG <jenniferzyy1012@gmail.com>
2019-11-21 19:09:36 -0500

    Update uk_elections.R

--- data/uk_lower/Elections/uk_elections.R
+++ data/uk_lower/Elections/uk_elections.R
@@ -23,5 +23,42 @@ lapply(c("stringr", "dplyr", "plyr", "tidyverse", "rvest", "zoo", "lubridate", "
 setwd("~/GitHub/CompLegFall2019/data/uk_lower/Elections")
 
 # Read downloaded data
-ele <- read.csv("1918-2017election_results.csv")
+elec <- read_csv("1918-2017election_results.csv")
 
+# Function for capitalizing the first letter or each word
+CapStr <- function(y) {
+  c <- strsplit(y, " ")[[1]]
+  paste(toupper(substring(c, 1, 1)), tolower(substring(c, 2)),
+        sep="", collapse=" ")
+}
+
+# Function to apply the previous function onto dataframe
+capitalize_str <- function(charcter_string){
+  sapply(charcter_string, CapStr)
+}
+
+# Reformat constituency
+ele <- elec %>%
+  mutate(Unconsted = as.numeric(total_votes<0)) %>%
+  #mutate(constituency=replace(constituency,constituency ==constituency[15097], "Ynys Môn")) %>%
+  mutate(constituency_name = capitalize_str(constituency))
+ele$total_votes[is.na(ele$turnout)] <- 0
+ele$turnout[is.na(ele$turnout)] <- 0
+# ele$constituency[15097] <- "Ynys Môn"
+ele$constituency_id <- NULL
+ele$X20 <- NULL
+ele$constituency <- NULL
+
+# Reformat constituency for better merge
+term <- rbind(c("&","and"),c("And","and"),c("Of","of"))
+for (i in 1:nrow(term)){
+  ele$constituency_name <- gsub(term[i,1],term[i,2], ele$constituency_name, perl = TRUE)
+}
+
+# Read constituency ID from previous dataset
+cons <- read_csv("../Constituencies/uk_lower_constituencies.csv") %>%
+  select(constituency_name,constituency_path)
+
+# Merge constituency ID
+check <- merge(ele,cons,by = "constituency_name", all = T) %>%
+  distinct()
